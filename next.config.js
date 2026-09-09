1f0520e228e0b1690a9c42d5ed3822bbfa7296da
James Brooks <james@jamesbrooks.io>
2026-09-09 11:51:21 +0100

    Enable Turbopack

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
-const withVanillaExtract = createVanillaExtractPlugin();
+const withVanillaExtract = createVanillaExtractPlugin({
+  unstable_turbopack: { mode: "auto" },
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
